c665580a2af219fac1ab3db888efcea71c122a55
MtFModMaker06 <96403109+MtFModMaker06@users.noreply.github.com>
2021-12-23 22:55:30 -0500

    Add files via upload

--- src/main/java/mod/Flags.java
+++ src/main/java/mod/Flags.java
@@ -1,28 +1,33 @@
 package mod;
 
 import net.minecraft.client.renderer.entity.PaintingRenderer;
-import net.minecraft.world.entity.decoration.Motive;
-import net.minecraft.world.entity.decoration.Painting;
+import net.minecraft.entity.item.PaintingType;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Flags{
- 
 
-    public static DeferredRegister<Motive> FLAGS = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, "pride_flags");
 
-    public static RegistryObject<Motive> GAY = FLAGS.register("gay", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> LESBIAN = FLAGS.register("lesbian", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> TRANS = FLAGS.register("trans", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> progress = FLAGS.register("progress", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> BISEXAUL = FLAGS.register("bisexual", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> PANSEXAUL = FLAGS.register("pansexual", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> ASEXAUL = FLAGS.register("asexual", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> NONBINARY = FLAGS.register("nonbinary", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> AGENDER = FLAGS.register("agender", () -> new Motive(64, 48));
-    public static RegistryObject<Motive> blm = FLAGS.register("blm", () -> new Motive(64, 64));
+    public static DeferredRegister<PaintingType> FLAGS = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, "pride_flags");
+
+    public static RegistryObject<PaintingType> GAY = FLAGS.register("gay", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> LESBIAN = FLAGS.register("lesbian", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> TRANS = FLAGS.register("trans", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> progress = FLAGS.register("progress", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> BISEXAUL = FLAGS.register("bisexual", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> PANSEXAUL = FLAGS.register("pansexual", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> ASEXAUL = FLAGS.register("asexual", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> NONBINARY = FLAGS.register("nonbinary", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> AGENDER = FLAGS.register("agender", () -> new PaintingType(64, 48));
+    public static RegistryObject<PaintingType> blm = FLAGS.register("blm", () -> new PaintingType(64, 64));
+
+
+
 
 
     public static void register() {
@@ -30,5 +35,7 @@ public class Flags{
 
         FLAGS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
+
+
 
 }
--- src/main/java/mod/PrideFlags.java
+++ src/main/java/mod/PrideFlags.java
@@ -1,21 +1,11 @@
 package mod;
-
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("pride_flags")
@@ -26,7 +16,6 @@ public class PrideFlags
 
     public PrideFlags() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
         Flags.register();
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -37,12 +26,7 @@ public class PrideFlags
     }
 
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
+
 
 
 }
